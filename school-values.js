b0437c018583ca25679b77d8be73caaeb99d70d2
QuickMind1624 <vvasu6956@gmail.com>
2026-08-20 13:34:31 +0530

    Add files via upload

---
+++ school-values.js
@@ -0,0 +1,48 @@
+const SCHOOL_VALUES = {
+
+    0: {
+        sin: "0",
+        cos: "1",
+        tan: "0",
+        cot: "undefined",
+        sec: "1",
+        cosec: "undefined"
+    },
+
+    30: {
+        sin: "1/2",
+        cos: "√3/2",
+        tan: "1/√3",
+        cot: "√3",
+        sec: "2/√3",
+        cosec: "2"
+    },
+
+    45: {
+        sin: "1/√2",
+        cos: "1/√2",
+        tan: "1",
+        cot: "1",
+        sec: "√2",
+        cosec: "√2"
+    },
+
+    60: {
+        sin: "√3/2",
+        cos: "1/2",
+        tan: "√3",
+        cot: "1/√3",
+        sec: "2",
+        cosec: "2/√3"
+    },
+
+    90: {
+        sin: "1",
+        cos: "0",
+        tan: "undefined",
+        cot: "0",
+        sec: "undefined",
+        cosec: "1"
+    }
+
+};
